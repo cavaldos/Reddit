4925cbc2a64092e20172740e49c21208c8ea71ba
cavaldos <khanhnguyen297kn@gmail.com>
2023-10-10 00:51:17 +0700

    init

--- client/src/app/signin/page.tsx
+++ client/src/app/signin/page.tsx
@@ -1,16 +1,26 @@
-'use client';
+"use client";
 
 //image next to the form
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 
 const SignPage: React.FC = () => {
   const url =
     "https://images.unsplash.com/photo-1696587522095-1d0b522b3e36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80";
 
   const handleSignIn = () => {
-      alert('Sign in')
+    alert("Sign in");
   };
+  const router = useRouter();
+
+  const auth = true;
+  console.log("khanh", auth);
+  if (!auth) {
+    router.push("/");
+    return <></>;
+  }
 
   return (
     <>
@@ -24,11 +34,7 @@ const SignPage: React.FC = () => {
               onClick={handleSignIn}
               className=" text-dark  flex  border-2 border-gray-950		 rounded-[7px] px-4 py-2 "
             >
-              <img
-                src="/icon/gg.svg"
-                alt="google"
-                className="w-6 h-6 mr-3"
-              />
+              <img src="/icon/gg.svg" alt="google" className="w-6 h-6 mr-3" />
               Sign in with Google
             </button>
           </div>

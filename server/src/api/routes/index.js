ae1f93ff9697f5027070e3a24164b8794716c95e
cavaldos <khanhnguyen297kn@gmail.com>
2023-10-14 02:28:46 +0700

    editor

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -3,11 +3,10 @@ const postRouter = require("./post.route");
 const userRouter = require("./user.route");
 const commentRouter = require("./comment.route");
 
-
 //export all routes
 module.exports = {
-    authRouter,
-    postRouter,
-    userRouter,
-    commentRouter
-}
+  authRouter,
+  postRouter,
+  userRouter,
+  commentRouter,
+};
